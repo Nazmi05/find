bd814cad846132bc633bd9dc05b792e5d21a47a9
Nazmi05 <narikoscarlet@gmail.com>
2024-10-28 16:21:01 +0600

    Add files via upload

---
+++ hw-5 find/main.js
@@ -0,0 +1,58 @@
+  /*const task1=function(){
+ const num=[1, 3, 7, 8, 10]
+
+ const findeven=num.find(item=>{
+     return item%2===0;
+ })
+
+console.log(findeven);
+ }
+*/
+
+/*const task2=function(){
+const string=['кот', 'собака', 'тигр', 'слон']
+
+const findstring=string.find(item=>{
+    return item.length>5;
+})
+
+console.log(findstring);
+}
+*/
+
+/*const task3=function(){
+const overten=[5, 9, 12, 3, 18]
+
+const findten=overten.find(item=>{
+    return item>10;
+})
+console.log(findten);
+}
+*/
+
+/*const task4=function(){
+  const users=[
+    { name: 'Алекс', age: 25 },
+    { name: 'Мария', age: 17 },
+    { name: 'Иван', age: 19 }
+  ]
+
+  const findMaria=users.find(item=>{
+    return item.name=='Мария';
+  })
+
+  console.log(findMaria)
+}
+  */
+
+
+/*const task5=function(){
+  const negative=[3, 5, -2, -8, 7]
+
+  const findnegative=negative.find(item=>{
+    return item<0;
+  })
+
+  console.log(findnegative);
+}
+*/
